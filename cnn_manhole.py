9efa3cad803d290625a8d3a4b01769fb17bf414e
Jose <jgdugo@gmail.com>
2019-06-19 18:50:02 +0200

    segundo entrnamiento, accuracy val 0.9595

--- cnn_manhole.py
+++ cnn_manhole.py
@@ -9,8 +9,8 @@ import numpy as np
 from numpy import genfromtxt
 
 # load data for training
-trainN = np.memmap("negative_train", dtype='float32', mode='r', shape=(80741*60, 80))
-trainP = np.memmap("positive_train", dtype='float32', mode='r', shape=(30321*60, 80))
+trainN = np.memmap("../negative_train", dtype='float32', mode='r', shape=(80741*60, 80))
+trainP = np.memmap("../positive_train", dtype='float32', mode='r', shape=(30321*60, 80))
 X_train = trainN
 X_train = np.append(X_train, trainP, axis=0)
 
@@ -19,8 +19,8 @@ Y_train = np.append(np.zeros(int(len(trainN)/60)),np.ones(int(len(trainP)/60)))
 # print ("Train samples: ", len(Y_train))
 
 # load data for testing
-testN = np.memmap("negative_test", dtype='float32', mode='r', shape=(8971*60, 80))
-testP = np.memmap("positive_test", dtype='float32', mode='r', shape=(3369*60, 80))
+testN = np.memmap("../negative_test", dtype='float32', mode='r', shape=(8971*60, 80))
+testP = np.memmap("../positive_test", dtype='float32', mode='r', shape=(3369*60, 80))
 X_test = testN
 X_test = np.append(X_test, testP, axis=0)
 X_test = np.reshape(X_test, (int(len(X_test)/60), 60, 80, 1))
@@ -32,6 +32,7 @@ Y_test = np.append(np.zeros(int(len(testN)/60)),np.ones(int(len(testP)/60)))
 # Model: (30, 5, 5), max, (30, 5, 5), max, (20, 3, 3), max, (10, 3, 3), flat, 150, 50, 1 ==> 
 model = Sequential()
 model.add(Convolution2D(30, 5, 5, border_mode='valid', input_shape=(60, 80, 1), activation='relu'))
+model.add(Convolution2D(30, 5, 5, border_mode='valid', activation='relu'))
 model.add(Dropout(0.2))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Convolution2D(30, 5, 5, border_mode='valid', activation='relu'))
@@ -48,7 +49,6 @@ model.add(Dropout(0.2))
 model.add(Dense(50, init='uniform', activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
-
 # Compile model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
@@ -56,11 +56,11 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 checkpointer = ModelCheckpoint(filepath="./model.{epoch:02d}-{acc:.2f}-{val_acc:.2f}.hdf5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False)
 
 # Fit the model
-history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=40, batch_size=100, callbacks=[checkpointer])
+history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=35, batch_size=64, callbacks=[checkpointer])
 #history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=10, batch_size=100)
 
 # Save model in file
-model.save("model_epoch_10.hdf5")
+model.save("model_epoch_11.hdf5")
 
 # Ploteo de loss y accuracy en el training y el subset de validacion
 # Loss = mse
